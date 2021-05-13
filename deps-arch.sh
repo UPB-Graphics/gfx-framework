0f219e2362506235ff11db4b47b877e2158164c5
CatalinPuscoci <32127810+CatalinPuscoci@users.noreply.github.com>
2021-05-13 19:59:07 +0300

    Update deps-arch.sh
    Change pacman flags to -Syu. Using -Sy is not recommended nor supported (https://wiki.archlinux.org/title/System_maintenance#Partial_upgrades_are_unsupported)

--- deps-arch.sh
+++ deps-arch.sh
@@ -3,12 +3,12 @@
 # Run this script as root, obviously
 
 # Should already be installed
-# pacman -Sy base-devel
+# pacman -Syu base-devel
 
 # It's assumed a functional graphics driver is installed and in-use, also X11
 # If glxgears works, you should be fine
 # https://wiki.archlinux.org/index.php/xorg
-pacman -Sy cmake assimp glm glew glfw-x11 mesa lib32-mesa
+pacman -Syu cmake assimp glm glew glfw-x11 mesa lib32-mesa
 
 # For proprietary nvidia drivers
-# pacman -Sy nvidia-utils lib32-nvidia-utils
+# pacman -Syu nvidia-utils lib32-nvidia-utils
