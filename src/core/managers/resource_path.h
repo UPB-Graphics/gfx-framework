273035b9f7e78ed6a8584dfcc7b7658144255d7d
flriancu <florineugen.iancu@gmail.com>
2021-07-13 21:56:18 +0300

    remove duplicate post-build events code

--- src/core/managers/resource_path.h
+++ src/core/managers/resource_path.h
@@ -7,7 +7,7 @@
 
 namespace RESOURCE_PATH
 {
-    const std::string ROOT      = PATH_JOIN("res");
+    const std::string ROOT      = PATH_JOIN("assets");
     const std::string MODELS    = PATH_JOIN(ROOT, "models");
     const std::string TEXTURES  = PATH_JOIN(ROOT, "textures");
     const std::string SHADERS   = PATH_JOIN(ROOT, "shaders");
